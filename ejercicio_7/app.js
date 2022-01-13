f7286cc3317d13af3f7f5c9c1854851ee5d4ca22
Adrià Mestres <amestreszk@gmail.com>
2022-01-13 21:44:50 +0100

    Solucion ejerciciio 7

--- ejercicio_7/app.js
+++ ejercicio_7/app.js
@@ -3,9 +3,13 @@ const obtenerPersonaFake = require("./library");
 
 // Codigo funcion callback
 
+const callback = (data) =>{
+    const persona = data.results[0]
 
+    console.log(`${persona.name.title} ${persona.name.first} ${persona.name.last}`);
+}
 
 // Fin codigo
 
 // Usar la funcion obtenerPersonaFake(funcionCallback) en donde funcionCallback es una función que recibe el objeto persona extraido
-obtenerPersonaFake();
+obtenerPersonaFake(callback);
