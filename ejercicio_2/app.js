0ece9ba3c6142d135749ae0c732e19f20819cac8
Adrià Mestres <amestreszk@gmail.com>
2022-01-13 20:36:10 +0100

    solucion ejercicio 2

--- ejercicio_2/app.js
+++ ejercicio_2/app.js
@@ -2,4 +2,22 @@
 const obtenerChiste = require("./library");
 
 // Usar la funcion obtenerChiste() la cual devuelve la promesa de traer el objeto chiste extraido
-obtenerChiste();
+
+obtenerChiste()
+    .then(function(response){
+        console.log(response.value)
+    })
+    .catch(function(error){
+        console.log(error)
+    });
+
+/*
+obtenerChiste()
+    .then(miCallback)
+    .catch(function(error){
+        console.log(error)
+    });
+
+function miCallback(response){
+    console.log(response.value)
+}*/
