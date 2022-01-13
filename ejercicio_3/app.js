a98f9f05516daabed43437d43b18271808994504
Fernando <fernando.montero.perez@gmail.com>
2022-01-13 19:39:58 +0000

    solucion ejercicio 3

--- ejercicio_3/app.js
+++ ejercicio_3/app.js
@@ -2,6 +2,8 @@ const obtenerChiste = require("./library");
 
 // Usar la funcion obtenerChiste() la cual devuelve la promesa de traer el objeto chiste extraido
 const app = async () => {
-  obtenerChiste();
+  const chiste = await obtenerChiste();
+
+  console.log("chiste: ", chiste.value);
 };
 app();
