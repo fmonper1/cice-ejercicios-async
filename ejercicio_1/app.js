59c9567d7fa5e5b93b3dd25ce6f4b91a36badba0
Fernando <fernando.montero.perez@gmail.com>
2022-01-13 19:11:33 +0000

    solucion ejercicio 1

--- ejercicio_1/app.js
+++ ejercicio_1/app.js
@@ -1,11 +1,16 @@
-
 const obtenerChiste = require("./library");
 
 // Codigo funcion callback
 
+const funcionCallback = (data) => {
+  console.log(data.value);
+};
 
+const callbackMayusculas = (data) => {
+  console.log(data.value.toUpperCase());
+};
 
 // Fin codigo
 
 // Usar la funcion obtenerChiste(funcionCallback) en donde funcionCallback es una función que recibe el objeto chiste extraido
-obtenerChiste();
+obtenerChiste(funcionCallback);
