5fc482655890f80145f1dd4f19844ed2f4faf7b2
Adrià Mestres <amestreszk@gmail.com>
2022-01-13 21:27:25 +0100

    Solucion ejercicio 6

--- ejercicio_6/app.js
+++ ejercicio_6/app.js
@@ -2,6 +2,16 @@ const obtenerPokemon = require("./library");
 
 // Usar la funcion obtenerChiste(nombrePokemon) la cual recibe como string un nombre de pokemon y devuelve la promesa de traer el objeto pokemon extraido
 const app = async () => {
-  obtenerPokemon();
+ try{
+  const pokemon = await obtenerPokemon("bulbasaur");
+  console.log(pokemon.name);
+  console.log(pokemon.abilities);
+ }
+ catch(error){
+  console.log("error!!!!");
+ }
 };
 app();
+
+
+
